a5afffbccc2b527719d729b2d8ae7473efb1a00d
WESpekrul <wes.pekrul.oli@gmail.com>
2025-03-26 13:13:01 -0300

    3 Commit

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  meuBooleano=false;
+
+  atualizaBooleano(valor: boolean){
+    this.meuBooleano = valor;
+  }
 
 }
